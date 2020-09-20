0f206b09f1f6e1e7a0cc54c1dbe548e2cd6e090f
paaja90 <61755727+paaja90@users.noreply.github.com>
2020-09-20 14:15:38 +0200

    Update models.py

--- project/climb/models.py
+++ project/climb/models.py
@@ -1,5 +1,6 @@
 from climb import db
-from datetime import datetime
+from datetime import datetime, date
+
 
 class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -7,7 +8,8 @@ class Record(db.Model):
     grade = db.Column(db.String(10), nullable=False)
     sector = db.Column(db.String(50))
     location = db.Column(db.String(50), nullable=False)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    style = db.Column(db.String(10))
+    date = db.Column(db.Date, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
         return f"Record('{self.name}', '{self.grade}', '{self.sector}', '{self.location}')"
