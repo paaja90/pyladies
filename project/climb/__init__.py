751f77e4422d2cd4ea2cd86f804e22d13029275b
paaja90 <61755727+paaja90@users.noreply.github.com>
2020-09-20 14:17:38 +0200

    Update __init__.py

--- project/climb/__init__.py
+++ project/climb/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '60ef47dfe22df53aa7d3fc4e9ee39c0c'
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///list.db'
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 from climb import routes 
